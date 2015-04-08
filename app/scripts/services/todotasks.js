93f602534787d87ab4d588e67de8368b101cd80f
njosefbeck <njosefbeck@gmail.com>
2015-04-08 13:34:27 -0500

    Continued attempts to fix firebase data not appearing -- implementing egghead tutorial solution for testing

--- app/scripts/services/todotasks.js
+++ app/scripts/services/todotasks.js
@@ -9,6 +9,29 @@
  */
 app.factory('toDoTasks', ['$firebaseArray', 'FIREBASE_URI', function ($firebaseArray, FIREBASE_URI) {
     var ref = new Firebase(FIREBASE_URI);
-    
-    return $firebaseArray(ref);
+    var tasks = $firebaseArray(ref);
+
+    var getTasks = function() {
+      return tasks;
+    };
+
+    var addTask = function(task) {
+      tasks.$add(task);
+    };
+
+    var updateTask = function(id) {
+      tasks.$save(id);
+    };
+
+    var removeTask = function(task) {
+      tasks.$remove(id);
+    };
+
+    return {
+      getTasks: getTasks,
+      addTask: addTask,
+      updateTask: updateTask,
+      removeTask: removeTask
+    }
+
 }]);
