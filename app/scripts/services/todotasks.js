f06d8dcf4e021b99f494d6713efb616f42d0fec2
njosefbeck <njosefbeck@gmail.com>
2015-04-08 12:37:21 -0500

    Added Firebase ref point

--- app/scripts/services/todotasks.js
+++ app/scripts/services/todotasks.js
@@ -8,5 +8,5 @@
  * Factory in the blocitoffApp.
  */
 app.factory('toDoTasks', ['$firebaseArray', 'FIREBASE_URI', function ($firebaseArray, FIREBASE_URI) {
-
+    var ref = new Firebase(FIREBASE_URI);
 }]);
