5aa912e0030f23d44fcffca68be18f11a9b55c63
njosefbeck <njosefbeck@gmail.com>
2015-04-08 12:09:41 -0500

    Removed code from previous attempts to refactor to use firebaseArray

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,13 +11,6 @@ angular.module('blocitoffApp')
   .controller('MainCtrl', ['$scope', 'toDoTasks',
     // Pass new toDoTasks factory into the controller
     function($scope, toDoTasks) {
-
-        // download active tasks into a local object
-        var syncActiveObject = $firebaseObject(activeIndexRef);
-
-        // synch object with a three-way data binding
-        syncActiveObject.$bindTo($scope, "active");
-      
+        
       };
-    }
   ]);
--- app/scripts/services/todotasks.js
+++ app/scripts/services/todotasks.js
@@ -8,18 +8,8 @@
  * Factory in the blocitoffApp.
  */
 angular.module('blocitoffApp')
-  .factory('toDoTasks', ['$firebaseObject', function ($firebaseObject) {
-        var url = 'https://popping-inferno-5377.firebaseio.com/';
-        var fbRef = new Firebase(url);
-        var tasksRef = fbRef.child('tasks');
-        var activeIndexRef = fbRef.child('users/nathan/active');
-        var completeIndexRef = fbRef.child('users/nathan/complete');
-        var expiredIndexRef = fbRef.child('users/nathan/expired');
+  .factory('toDoTasks', ['$firebaseArray', function ($firebaseArray) {
+      
 
-        // return each ref as a synchronized object
-        //return $firebaseObject(tasksRef);
-        //return $firebaseObject(activeIndexRef);
-        //return $firebaseObject(completeIndexRef);
-        //return $firebaseObject(expiredIndexRef);
-      }
+    }
   ]);
