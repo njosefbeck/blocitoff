4f91a4cb8f199886397eb74a85334f18c4583c57
njosefbeck <njosefbeck@gmail.com>
2015-04-14 18:56:54 -0500

    Began shaping completedTasks view

--- app/scripts/controllers/completedtasks.js
+++ app/scripts/controllers/completedtasks.js
@@ -9,9 +9,5 @@
  */
 angular.module('blocitoffApp')
   .controller('CompletedtasksCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+      
   });
