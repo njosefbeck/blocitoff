e1fbbb5785abd3e3682fc0070e0720ba67929cf7
njosefbeck <njosefbeck@gmail.com>
2015-04-23 11:09:05 -0500

    Attempt #2: Hide expired tasks

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,15 +20,28 @@ app.controller('MainCtrl', ['$scope', 'toDoTasks', '$timeout', function($scope, 
     $scope.newTask = { desc: '', priority: '', completed: false, created_on: Date() };
   };
 
-  $scope.hideExpired = function(task) {
-    if ($scope.task.created_on > Date.now) {
-      $scope.task.completed = true;
+  $scope.cleanupTasks = function() {
+    var timediff = 0;
+    var d1 = new Date();
+    var d2;
+
+    for (var i= 0; i < $scope.tasks.length; i++) {
+      d2 = new Date($scope.tasks[i].created_on);
+      timediff = d1 - d2;
+
+      console.log(timediff);
+
+      if (timediff > 10000){
+        $scope.tasks[i].completed = true;
+        $scope.tasks.$save();
+      }
     }
   };
 
-  //$timeout( function(){
-  //  $scope.hideExpired();
-  //}, 5000);
+  $timeout( function(){
+    console.log('ping');
+    $scope.cleanupTasks();
+  }, 5000);
 
   $scope.filterActiveTasks = function(task) {
     if (task.completed === true) {
